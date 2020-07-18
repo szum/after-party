816d4d8f9af4490244df91bd3fa42f9edc12d9b4
Lukas Swietlicki <lukas.switlicki@gmail.com>
2020-07-18 13:40:28 -0400

    Add aws configuration to overriden App component to configure auth upon initialization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,29 @@
-import { API, graphqlOperation } from '@aws-amplify/api';
+import { API } from 'aws-amplify';
 import * as queries from '../graphql/queries';
 
-export default function HomePage() {
-  return <div>Welcome to Next.js!</div>
+export default function HomePage(props) {
+  return(
+    <div>
+      {
+        props.rsvps.map((rsvp) => {
+          return (<div>{rsvp.firstName}, {rsvp.lastName}</div>);
+        })
+      }
+    </div>
+  );
+
 }
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
-  const rsvpData = API.graphqlOperation(queries.listRsvps);
+  const response = await API.graphql({ query: queries.listRsvps });
+  const rsvps = await response.data.listRsvps.items;
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      rsvpData
+      rsvps
     }
   }
 }
